c079a7e5e66d52144402ce491879e11ec817066a
SenRa User <SenRaUser@apples-MacBook-Pro.local>
2024-03-13 11:35:45 +0530

    modified package.json to handle test and lint together

--- server.test.js
+++ server.test.js
@@ -1,4 +1,3 @@
-// server.test.js
 import { expect } from 'chai';
 import supertest from 'supertest';
 import { before, after, it } from 'mocha';
@@ -16,8 +15,6 @@ before(async () => {
 after(async () => {
   // Close the Apollo Server
   await apolloServer?.stop();
-
-  // Close the Express server after all tests
   server.close(() => {
     console.log('Server closed');
   });
@@ -28,11 +25,5 @@ it('should expose GraphQL endpoint at /graphql', async () => {
   const response = await supertest(server).get('/graphql');
   console.log('Response:', response.body);
   expect(response.status).to.equal(400);
-  // Add more assertions based on your GraphQL server setup
-
-  // Example:
-  // expect(response.body).to.have.property('data');
-  // expect(response.body.data).to.have.property('yourQueryOrMutation');
 });
 
-// Add more tests as needed
